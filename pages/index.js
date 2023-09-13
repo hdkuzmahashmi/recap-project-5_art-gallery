b9601019e9043a8751a98ebf6bbf99aad848630e
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-13 10:36:20 +0200

    implement user-story-3

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,11 @@
-import useSWR from "swr";
-
-import ArtPieces from "@/components/ArtPieces";
 import Spotlight from "@/components/Spotlight";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-export default function HomePage() {
-  const { data, error, isLoading } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
-  );
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
-
+export default function SpotlightPage({ data }) {
   const randomArt = data[Math.floor(Math.random() * data.length)];
 
   return (
     <div>
       <h1>Art Gallery</h1>
-      <ArtPieces pieces={data} />
       <Spotlight
         image={randomArt.imageSource}
         artist={randomArt.artist}
